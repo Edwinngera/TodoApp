5cf3b402e70460b055c28f93da21fd76702c0cf7
Edwinngera <edwinngera@gmail.com>
2023-09-25 01:22:48 +0300

    first commit

---
+++ lib/screens/add_page.dart
@@ -0,0 +1,71 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class AddTodoPage extends StatefulWidget {
+  const AddTodoPage({super.key});
+
+  @override
+  State<AddTodoPage> createState() => _AddTodoPageState();
+}
+
+class _AddTodoPageState extends State<AddTodoPage> {
+  TextEditingController titleController = TextEditingController();
+  TextEditingController descriptionController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Add Todo'),
+        ),
+        body: ListView(
+          padding: EdgeInsets.all(20),
+          children: [
+            TextField(
+              controller: titleController,
+              decoration: InputDecoration(hintText: 'Title'),
+            ),
+            SizedBox(height: 20),
+            TextField(
+              controller: descriptionController,
+              decoration: InputDecoration(hintText: 'Description'),
+              keyboardType: TextInputType.multiline,
+              minLines: 5,
+              maxLines: 8,
+            ),
+            ElevatedButton(onPressed: submitData, child: Text('Submit'))
+          ],
+        ));
+  }
+
+  Future<void> submitData() async {
+    // Get Data from the form
+    final title = titleController.text;
+    final description = descriptionController.text;
+    //Submit data to the server
+    final body = {
+      "title": title,
+      "description": description,
+      "is_completed": false
+    };
+
+    final url = "https://api.nstack.in/v1/todos";
+    final uri = Uri.parse(url);
+    final response = await http.post(uri,
+        body: jsonEncode(body), headers: {'Content-Type': 'application/json'});
+    //Show success or fail message
+    if (response.statusCode == 200) {
+      print("Creation Success");
+    } else {
+      print('Creation Failed');
+      print(response.body);
+    }
+  }
+
+
+
+
+
+}
